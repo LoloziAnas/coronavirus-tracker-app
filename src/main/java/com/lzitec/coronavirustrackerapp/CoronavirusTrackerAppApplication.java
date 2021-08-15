b9cf265d65068429a87f7911525944c80e3370b1
LoloziAnas <lolozianas1@gmail.com>
2021-08-15 04:46:02 +0100

    first commit

---
+++ src/main/java/com/lzitec/coronavirustrackerapp/CoronavirusTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.lzitec.coronavirustrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronavirusTrackerAppApplication.class, args);
+    }
+
+}
